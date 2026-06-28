7689e144ca901d44fbe0235963462e2281447c22
Vivekanandareddykotla <95586764+Vivekanandareddykotla@users.noreply.github.com>
2026-06-28 00:55:36 -0700

    Update env2.py

--- chapter-1/env2.py
+++ chapter-1/env2.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World"
+    return "MY First Own Project"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
